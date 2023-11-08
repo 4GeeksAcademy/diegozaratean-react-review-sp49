00f78da192467fd6c2808d439d4b7a220d1c0028
diego <diegozaratean@gmail.com>
2023-11-08 16:04:30 +0000

    finish react review clas

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Cart from "./component/cart.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Cart cant='2' />, document.querySelector("#app"));
